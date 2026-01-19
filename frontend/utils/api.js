39f73882293e4cd0f7c3072c2d99a3ae4b7366ec
rithvik318 <153345925+rithvik318@users.noreply.github.com>
2026-01-20 00:19:54 +0530

    Fail build if NEXT_PUBLIC_API_URL is missing

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,4 +1,7 @@
-// frontend/utils/api.js
 const API_BASE = process.env.NEXT_PUBLIC_API_URL;
+
+if (!API_BASE) {
+  throw new Error("NEXT_PUBLIC_API_URL is not set at build time");
+}
 
 export default API_BASE;
